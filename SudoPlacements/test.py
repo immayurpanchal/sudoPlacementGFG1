aca1facd09f625ae071c273bb6383644d25ce39e
Mayur Panchal <mayurpanchal@PUN-IT000540.local>
2019-07-05 13:38:15 +0530

    gitignore updated

--- SudoPlacements/test.py
+++ SudoPlacements/test.py
@@ -1,3 +1,7 @@
 # print('hello world')
-values = [int(x) for x in input().split()]
-print(values)
+# values = [int(x) for x in input().split()]
+# print(values)
+# print(len(values))
+arr = [1,2,3,4,5]
+rot = 2
+print(arr[rot::]+arr[:rot:])
