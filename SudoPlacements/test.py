34aec74f2935005965f028cc0ad962a9eb1bd0bc
immayurpanchal <mayurpanchal002@gmail.com>
2019-07-09 19:41:14 +0530

    test updated

--- SudoPlacements/test.py
+++ SudoPlacements/test.py
@@ -2,6 +2,9 @@
 # values = [int(x) for x in input().split()]
 # print(values)
 # print(len(values))
-arr = [1,2,3,4,5]
-rot = 2
-print(arr[rot::]+arr[:rot:])
+# arr = [1,2,3,4,5]
+# rot = 2
+# print(arr[rot::]+arr[:rot:])
+size = 5 
+for index in range(size):
+    print(index)
